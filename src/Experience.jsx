40f1f4d4f51f2ea2753e6474e6e40fb02dc3e239
Rafaela <104620799+rafaeladev@users.noreply.github.com>
2024-04-01 19:45:24 +0200

    [Add] Decal

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,140 +1,146 @@
 // Import pour l'animation
-import { useRef } from "react";
+import { useRef } from 'react';
 
 // Helpers de Drei pour la scene
 import {
-  shaderMaterial,
-  Sparkles,
-  Center,
-  useTexture,
-  useGLTF,
-  OrbitControls,
-  Environment,
-  Text,
-  Box,
-  Float,
-} from "@react-three/drei";
+    shaderMaterial,
+    Sparkles,
+    Center,
+    useTexture,
+    useGLTF,
+    OrbitControls,
+    Environment,
+    Text,
+    Box,
+    Float,
+} from '@react-three/drei';
 
-import { useControls } from "leva";
+import { useControls } from 'leva';
 
 // Import pour avoir les couleurs de THREE.js
-import * as THREE from "three";
-import { useState } from "react";
+import * as THREE from 'three';
+import { useState } from 'react';
 
 // import des fonctionnalités React pour Three (donc issues de react three fiber)
-import { extend, useFrame } from "@react-three/fiber";
+import { extend, useFrame } from '@react-three/fiber';
 
-import Planes from "/Planes.jsx";
+import Planes from '/Planes.jsx';
 
 export default function Experience() {
-  const [isMoving, setIsMoving] = useState(false);
-  const [targetHeight, setTargetHeight] = useState(1);
-  const model = useGLTF("./model/POC_Maquette.glb");
-  // console.log(model);
+    const [isMoving, setIsMoving] = useState(false);
+    const [targetHeight, setTargetHeight] = useState(1);
+    const model = useGLTF('./model/POC_Maquette.glb');
+    // console.log(model);
 
-  const eau_int = useRef();
-  const buttonCube = useRef();
-  const meshRef = useRef();
-  const camera = useRef();
+    const eau_int = useRef();
+    const buttonCube = useRef();
+    const meshRef = useRef();
+    const camera = useRef();
 
-  const targetFile = import("/panda.jpg").then((module) => module.default);
+    const targetFile = import('/panda.jpg').then((module) => module.default);
 
-  // Debug
-  const { rotation } = useControls({
-    rotation: {
-      value: { x: 0, y: 10, z: 0 },
-      step: 1,
-    },
-  });
+    // Debug
+    const { rotation } = useControls({
+        rotation: {
+            value: { x: 0, y: 10, z: 0 },
+            step: 1,
+        },
+    });
 
-  // Models
-  const iphoneModel = useGLTF("/model/iphone.gltf");
-  const nintendoModel = useGLTF("/model/nintendo.gltf");
-  const pcModel = useGLTF("/model/pc.gltf");
+    // Models
+    const iphoneModel = useGLTF('/model/iphone.gltf');
+    const nintendoModel = useGLTF('/model/nintendo.gltf');
+    const pcModel = useGLTF('/model/pc.gltf');
 
-  console.log(iphoneModel);
+    console.log(iphoneModel);
 
-  const restart = () => {
-    console.log("restart");
-    setIsMoving(true);
-    setTargetHeight(targetHeight === 1 ? 0 : 1);
-  };
+    const restart = () => {
+        console.log('restart');
+        setIsMoving(true);
+        setTargetHeight(targetHeight === 1 ? 0 : 1);
+    };
 
-  useFrame((state) => {
-    console.log(state.camera.position);
-    if (isMoving) {
-      const currentY = eau_int.current.position.y;
-      const deltaY = (targetHeight - currentY) * 0.1; // Vitesse de déplacement
+    useFrame((state) => {
+        // console.log(state.camera.position);
+        if (isMoving) {
+            const currentY = eau_int.current.position.y;
+            const deltaY = (targetHeight - currentY) * 0.1; // Vitesse de déplacement
 
-      if (Math.abs(deltaY) < 0.001) {
-        setIsMoving(false);
-      } else {
-        eau_int.current.position.y += deltaY;
-      }
-    }
-  });
+            if (Math.abs(deltaY) < 0.001) {
+                setIsMoving(false);
+            } else {
+                eau_int.current.position.y += deltaY;
+            }
+        }
+    });
 
-  const handlePress = () => {
-    buttonCube.current.position.y -= 0.1; // Déplace le bouton vers le bas lorsque pressé
-  };
+    const handlePress = () => {
+        buttonCube.current.position.y -= 0.1; // Déplace le bouton vers le bas lorsque pressé
+    };
 
-  const handleRelease = () => {
-    buttonCube.current.position.y += 0.1; // Remet le bouton à sa position initiale lorsque relâché
-    restart(); // Démarre l'animation lorsque le bouton est relâché
-  };
+    const handleRelease = () => {
+        buttonCube.current.position.y += 0.1; // Remet le bouton à sa position initiale lorsque relâché
+        restart(); // Démarre l'animation lorsque le bouton est relâché
+    };
 
-  return (
-    <>
-      <Environment path="/envMap/" files="potsdamer_platz_256.hdr" />
-      <color args={["#241B27"]} attach="background" />
-      <OrbitControls makeDefault />
-      <directionalLight
-        castShadow
-        position={[1, 2, 3]}
-        intensity={0.2}
-        shadow-normalBias={0.04}
-      />
-      <ambientLight intensity={0.1} />
+    return (
+        <>
+            <Environment
+                path='/envMap/'
+                files='potsdamer_platz_256.hdr'
+            />
+            <color
+                args={['#241B27']}
+                attach='background'
+            />
+            <OrbitControls makeDefault />
+            <directionalLight
+                castShadow
+                position={[1, 2, 3]}
+                intensity={0.2}
+                shadow-normalBias={0.04}
+            />
+            <ambientLight intensity={0.1} />
 
-      <Center>
-        <Planes />
+            <Center>
+                <Planes />
 
-        {/* iphone model */}
-        <Float rotationIntensity={1}>
-          <primitive
-            object={iphoneModel.scene}
-            scale={2}
-            position-x={-14}
-            position-y={0.1}
-            rotation-y={-1}
-            rotation-x={0}
-            receiveShadow
-          ></primitive>
-        </Float>
+                {/* iphone model */}
+                <Float rotationIntensity={1}>
+                    <primitive
+                        object={iphoneModel.scene}
+                        scale={2}
+                        position-x={-14}
+                        position-y={0.1}
+                        rotation-y={-1}
+                        rotation-x={0}
+                        receiveShadow
+                    ></primitive>
+                </Float>
 
-        {/*  Cube */}
-        <mesh
-          ref={buttonCube}
-          position-x={-10}
-          position-y={-1.5}
-          position-z={1}
-          rotation-y={rotation.y}
-          rotation-x={rotation.x}
-          scale={1}
-          onClick={restart}
-          onPointerDown={handlePress} // Appelé lorsque le bouton est pressé
-          onPointerUp={handleRelease} // Appelé lorsque le bouton est relâché
-          onPointerEnter={() => {
-            document.body.style.cursor = "pointer";
-          }}
-          onPointerLeave={() => {
-            document.body.style.cursor = "default";
-          }}
-        >
-          <boxGeometry args={[0.5, 2, 3]} />
-          <meshStandardMaterial color="purple" />
-        </mesh>
-      </Center>
-    </>
-  );
+                {/*  Cube */}
+                <mesh
+                    ref={buttonCube}
+                    position-x={-10}
+                    position-y={-1.5}
+                    position-z={1}
+                    rotation-y={rotation.y}
+                    rotation-x={rotation.x}
+                    scale={1}
+                    onClick={restart}
+                    onPointerDown={handlePress} // Appelé lorsque le bouton est pressé
+                    onPointerUp={handleRelease} // Appelé lorsque le bouton est relâché
+                    onPointerEnter={() => {
+                        document.body.style.cursor = 'pointer';
+                    }}
+                    onPointerLeave={() => {
+                        document.body.style.cursor = 'default';
+                    }}
+                >
+                    <boxGeometry args={[0.5, 2, 3]} />
+                    <meshStandardMaterial color='purple' />
+                </mesh>
+            </Center>
+        </>
+    );
 }
--- src/Planes.jsx
+++ src/Planes.jsx
@@ -1,62 +1,78 @@
-import React, { useRef } from "react";
-import { useLoader } from "@react-three/fiber";
-import { TextureLoader } from "three";
-import { RoundedBox } from "@react-three/drei";
-import * as THREE from "three";
+import React, { useRef } from 'react';
+import { useLoader } from '@react-three/fiber';
+import { TextureLoader } from 'three';
+import { RoundedBox, Decal, RenderTexture, PerspectiveCamera, Text } from '@react-three/drei';
+import * as THREE from 'three';
 
 const Experience = () => {
-  // These refs will give us direct access to the THREE.Mesh object
-  const rectOneRef = useRef();
-  const rectTwoRef = useRef();
-  const rectThreeRef = useRef();
+    // These refs will give us direct access to the THREE.Mesh object
+    const rectOneRef = useRef();
+    const rectTwoRef = useRef();
+    const rectThreeRef = useRef();
+    const textRef = useRef();
 
-  // Use TextureLoader to load button textures
-  const buttonTexture1 = useLoader(TextureLoader, "/texture/8139482.jpg");
-  const buttonTexture2 = useLoader(TextureLoader, "/texture/8139482.jpg");
-  const buttonTexture3 = useLoader(TextureLoader, "/texture/8139482.jpg");
+    // Use TextureLoader to load button textures
+    const buttonTexture1 = useLoader(TextureLoader, '/texture/8139482.jpg');
+    const buttonTexture2 = useLoader(TextureLoader, '/texture/8139482.jpg');
+    const buttonTexture3 = useLoader(TextureLoader, '/texture/8139482.jpg');
 
-  // Create a mesh for each rectangle with a MeshBasicMaterial
-  // Position them according to your needs to match the image layout
+    // Create a mesh for each rectangle with a MeshBasicMaterial
+    // Position them according to your needs to match the image layout
 
-  return (
-    <>
-      <RoundedBox
-        ref={rectOneRef}
-        radius={0.2}
-        smoothness={4}
-        args={[5.5, 8, 0]}
-        position={[-6, 0, 0]}
-      >
-        {/* You can use a mesh basic material and map a texture to it */}
-        <meshBasicMaterial attach="material" map={buttonTexture1} />
-        <meshStandardMaterial color="skyBlue" />
-      </RoundedBox>
+    return (
+        <>
+            <RoundedBox
+                ref={rectOneRef}
+                radius={0.21}
+                smoothness={4}
+                args={[5.5, 8, 1]}
+                position={[-6, 0, 0]}
+            >
+                {/* You can use a mesh basic material and map a texture to it */}
+                {/* <meshBasicMaterial
+                    attach='material'
+                    map={buttonTexture1}
+                /> */}
+                <meshStandardMaterial color='skyBlue' />
+                <Decal
+                    // debug
+                    position={[0, 0, 0.5]}
+                    rotation={[0, 0, 0]}
+                    scale={[8, 8, 1]}
+                >
+                    <meshBasicMaterial
+                        map={buttonTexture1}
+                        polygonOffset
+                        polygonOffsetFactor={-1}
+                    />
+                </Decal>
+            </RoundedBox>
 
-      <RoundedBox
-        ref={rectTwoRef}
-        radius={0.2}
-        smoothness={4}
-        args={[5, 12]}
-        position={[0, 3, 0]}
-      >
-        {/* <meshBasicMaterial attach="material" map={buttonTexture2} /> */}
-        <meshStandardMaterial color="skyBlue" />
-      </RoundedBox>
+            <RoundedBox
+                ref={rectTwoRef}
+                radius={0.2}
+                smoothness={4}
+                args={[5, 12, 1]}
+                position={[0, 3, 0]}
+            >
+                {/* <meshBasicMaterial attach="material" map={buttonTexture2} /> */}
+                <meshStandardMaterial color='skyBlue' />
+            </RoundedBox>
 
-      <RoundedBox
-        ref={rectThreeRef}
-        radius={0.2}
-        smoothness={4}
-        args={[5, 12]}
-        position={[6, 3, 0]}
-      >
-        {/*  <meshBasicMaterial attach="material" map={buttonTexture3} /> */}
-        <meshStandardMaterial color="skyBlue" />
-      </RoundedBox>
+            <RoundedBox
+                ref={rectThreeRef}
+                radius={0.2}
+                smoothness={4}
+                args={[5, 12, 1]}
+                position={[6, 3, 0]}
+            >
+                {/*  <meshBasicMaterial attach="material" map={buttonTexture3} /> */}
+                <meshStandardMaterial color='skyBlue' />
+            </RoundedBox>
 
-      {/* Similarly add interactive buttons using mesh with onClick handlers if needed */}
-    </>
-  );
+            {/* Similarly add interactive buttons using mesh with onClick handlers if needed */}
+        </>
+    );
 };
 
 export default Experience;
